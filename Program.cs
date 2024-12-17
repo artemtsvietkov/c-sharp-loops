c0635fb048d9eba7ced034a7f26da3848d59df38
アルテム <143085970+artemtsvietkov@users.noreply.github.com>
2024-12-17 13:34:42 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+
+    {
+         
+        // for(int number = 0 ;number <= 100; number++)
+        // {
+        //     if number % 2 == 0:
+        //         Console.WriteLine(number);
+        // }
+
+        
+
+    }
+}
