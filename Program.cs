d8428cd3ac6c72649b4a33ec259c836dd4f8c34d
アルテム <143085970+artemtsvietkov@users.noreply.github.com>
2024-12-17 13:36:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+
+    {
+         Uppgift 1
+        // for(int number = 0 ;number <= 100; number++)
+        // {
+        //     if number % 2 == 0:
+        //         Console.WriteLine(number);
+        // }
+         Uppgift 2
+        // for (int i = 10; i > 0; i--)
+        // {
+        //     string line = new string('*', i);
+        //     Console.WriteLine(line);
+        // }
+         Uppgift 3
+        // string correctPassword = "hejhej";
+        // int attempts = 0;
+        // string userGuess;
+
+        // Console.WriteLine("Gissa lösenordet:");
+
+        // do
+        // {
+        //     userGuess = Console.ReadLine();
+        //     attempts++;
+
+        //     if (userGuess != correctPassword)
+        //     {
+        //         Console.WriteLine("Fel lösenord. Försök igen:");
+        //     }
+
+        // } while (userGuess != correctPassword);
+
+        // Console.WriteLine($"Rätt lösenord! Det tog dig {attempts} försök.");
+
+        Uppgift 4
+
+        // Console.WriteLine("Ange ditt saldo:");
+        // double saldo = Convert.ToDouble(Console.ReadLine());
+
+        // Console.WriteLine("Ange ditt sparmål:");
+        // double slutmål = Convert.ToDouble(Console.ReadLine());
+
+        // Console.WriteLine("Ange sparränta i procent:");
+        // double sparranta = Convert.ToDouble(Console.ReadLine());
+
+        // double ranta = sparranta / 100;
+        // int ar = 0;
+
+        // while (saldo < slutmål)
+        // {
+        //     saldo += saldo * ranta; 
+        //     ar++;
+        // }
+
+        // Console.WriteLine($"Det tar {ar} år att nå ditt sparmål.");
+
+    }
+}
